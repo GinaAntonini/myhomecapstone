9a31ddc9d87055dcd1105dc3ad634e43673eae72
Gina Antonini <ginamantonini@gmail.com>
2017-12-04 22:18:12 -0600

    added routes in appconfig

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -3,3 +3,44 @@
 app.run(function($location, $rootScope, FIREBASE_CONFIG){
     firebase.initializeApp(FIREBASE_CONFIG);
 });
+
+app.config(function($routeProvider){
+    $routeProvider
+      .when("/create/newtask", {
+        templateUrl: 'partials/create/newtask.html',
+        controller: 'NewTaskCtrl',
+      })
+      .when("/create/newproject", {
+        templateUrl: 'partials/create/newproject.html',
+        controller: 'NewProjectCtrl',
+      })
+      .when("/maintenance/viewtasks", {
+        templateUrl: 'partials/maintenance/viewtasks.html',
+        controller: 'ViewTasksCtrl',
+      })
+      .when("/maintenance/seasonalview", {
+        templateUrl: 'partials/maintenance/maintseasonal.html',
+        controller: 'MaintSeasonalCtrl',
+      })
+      .when("/maintenance/archives", {
+        templateUrl: 'partials/maintenance/maintarchives.html',
+        controller: 'MaintArchivesCtrl',
+      })
+      .when("/improvements/viewprojects", {
+        templateUrl: 'partials/improvements/viewprojects.html',
+        controller: 'ViewProjectsCtrl',
+      })
+      .when("/improvements/seasonal", {
+        templateUrl: 'partials/improvements/improveseasonal.html',
+        controller: 'ImproveSeasonalCtrl',
+      })
+      .when("/improvements/archives", {
+        templateUrl: 'partials/improvements/improvearchives.html',
+        controller: 'ImproveArchivesCtrl',
+      })          
+      .when("/login", {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise('/login');
+  });
