fabb2d1e63593ad29bee20322a2757ddd67386c5
Gina Antonini <ginamantonini@gmail.com>
2017-12-05 20:58:25 -0600

    user can view completed tasks in the maintenance archives

--- javascripts/controllers/MaintArchivesCtrl.js
+++ javascripts/controllers/MaintArchivesCtrl.js
@@ -1,5 +1,15 @@
 "use strict";
 
-app.controller("MaintArchivesCtrl", function($location, $rootScope, $scope){
+app.controller("MaintArchivesCtrl", function($location, $rootScope, $scope, TaskService){
+
+    const getCompletedTasks = () => {
+    	TaskService.getCompletedTasksFromFirebase($rootScope.uid).then((results) => {
+            $scope.completed = results;
+            $location.path('/maintenance/archives');
+    	}).catch((err) => {
+    	console.log("error in getCompletedTasks", err);
+   	});
+   };
+   getCompletedTasks();
 
 });
