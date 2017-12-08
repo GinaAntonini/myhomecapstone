d9d195ace64edcec1096273b70ba1457c19df1bd
Gina Antonini <ginamantonini@gmail.com>
2017-12-07 20:41:43 -0600

    user can view completed projects in improvements/archives

--- javascripts/services/ProjectService.js
+++ javascripts/services/ProjectService.js
@@ -18,8 +18,27 @@ app.service("ProjectService", function($http, $q, FIREBASE_CONFIG){
     });
   };
 
+  const getCompletedProjectsFromFirebase = (userUid) => {
+		let projects = [];
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/improvements.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
+        let fbProjects = results.data;
+        Object.keys(fbProjects).forEach((key) => {
+          if (fbProjects[key].completed){
+            fbProjects[key].id = key;
+            projects.push(fbProjects[key]);
+          }
+        });
+        resolve(projects);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  };
+
 	const postNewImprovementProject = (newProject) => {
 		return $http.post(`${FIREBASE_CONFIG.databaseURL}/improvements.json`, JSON.stringify(newProject));
-    };
-    return {postNewImprovementProject, getCurrentProjectsFromFirebase};
+  };
+
+    return {postNewImprovementProject, getCurrentProjectsFromFirebase, getCompletedProjectsFromFirebase};
 });
