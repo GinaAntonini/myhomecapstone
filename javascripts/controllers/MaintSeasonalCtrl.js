89bdb4b96b1f52bb816531789b14a96b157a8bbd
Gina Antonini <ginamantonini@gmail.com>
2017-12-07 19:29:48 -0600

    trying to get seasonalview to work

--- javascripts/controllers/MaintSeasonalCtrl.js
+++ javascripts/controllers/MaintSeasonalCtrl.js
@@ -4,7 +4,6 @@ app.controller("MaintSeasonalCtrl", function($http, $location, $rootScope, $scop
     const getTasks = () => {
         TaskService.getCurrentTasksFromFirebase($rootScope.uid).then((results) => {
         $scope.tasks = results;
-        console.log("tasks", results);
         $location.url("/maintenance/seasonalview");
     }).catch((err) => {
         console.log("error in getTasks", err);
@@ -12,7 +11,28 @@ app.controller("MaintSeasonalCtrl", function($http, $location, $rootScope, $scop
     };
     getTasks();
 
-    $scope.markTaskCompleted = (taskId) => {
-        
+    const getSeasonToComplete = () => {
+        TaskService.getSeasonsFromFirebase($rootScope.uid).then((results) => {
+        $scope.seasonToComplete = results;
+    }).catch((err) => {
+        console.log("error in getSeasonToComplete", err);
+    });
+
+    $scope.getSeason = (event) => {
+        if(event.target.value === "Spring"){
+            getSeasonToComplete();
+        }
+        // } else (event.target.value === "Summer"){
+        //     getTasks();
+        // } else (event.target.value === "Fall"){
+        //     getTasks();
+        // } else (event.target.value === "Winter"){
+        //     getTasks();
+        console.log("event", event.target.value);
+        };
     };
+
+     // $scope.markTaskCompleted = (taskId) => {
+        
+    // };
 });
