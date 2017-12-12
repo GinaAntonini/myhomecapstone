b301947ebe2e17b12b936b5c3ecb3e4d9e9b22c6
Gina Antonini <ginamantonini@gmail.com>
2017-12-11 18:30:08 -0600

    decided not to have users edit from detail view

--- javascripts/controllers/NewProjectCtrl.js
+++ javascripts/controllers/NewProjectCtrl.js
@@ -5,7 +5,7 @@ app.controller("NewProjectCtrl", function($location, $rootScope, $scope, Project
         $scope.newProject.uid = $rootScope.uid;
         ProjectService.postNewImprovementProject($scope.newProject).then((results) => {
             $scope.newProject = {};
-            $scope.newProject.dateToBeCompleted = moment(results.data.dateToBeCompleted);
+            $scope.newProject.dateToBeCompleted = moment(results.data.dateToBeCompleted).format("MM-DD-YYYY");
                 $location.url("/improvements/viewprojects");
         }).catch((error) => {
             console.log("error in addNewProjectToFirebase", error);
--- javascripts/controllers/ProjectDetailCtrl.js
+++ javascripts/controllers/ProjectDetailCtrl.js
@@ -4,10 +4,10 @@ app.controller("ProjectDetailCtrl", function($location, $rootScope, $routeParams
     const getProject = () => {
         ProjectService.getSingleImprovementProject($routeParams.id).then((results) =>{
             $scope.project = results.data;
+            $scope.project.dateToBeCompleted = moment(results.data.dateToBeCompleted);
             }).catch((err) =>{
             console.log("err in getProject", err);
         });
     };
     getProject();
-
 });
