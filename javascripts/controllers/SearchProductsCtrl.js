700d13424f890c301e0ec0ff60a95eb0b38492c1
Gina Antonini <ginamantonini@gmail.com>
2017-12-15 16:24:40 -0600

    walmart api calls working and image, item name, and price are displaying when searched

--- javascripts/controllers/SearchProductsCtrl.js
+++ javascripts/controllers/SearchProductsCtrl.js
@@ -1,11 +1,13 @@
 "use strict";
 
-app.controller("SearchProductsCtrl", function($location, $rootScope, $scope, AmazonService){
-    $scope.products = [];
+app.controller("SearchProductsCtrl", function($location, $rootScope, $scope, WalmartService){
+    $scope.items = [];
+
     $scope.onEnterSearch = (event) => {
         if(event.keyCode === 13){
-            AmazonService.searchProducts(event.target.value).then((results) => {
-                $scope.products = results.data.results;
+            WalmartService.searchProducts(event.target.value).then((results) => {
+                $scope.items = results.data.items;
+                console.log(results.data.items);
             }).catch((err) => {
                 console.log("error in searchProducts", err);
             });
--- javascripts/services/AmazonService.js
+++ javascripts/services/AmazonService.js
@@ -1,20 +1,12 @@
-"use strict";
+// "use strict";
 
-app.service("AmazonService", function($http, AmazonSignerService){
+// app.service("AmazonService", function($http, AmazonSignerService){
 
-    const searchProducts = (query) => {
-        AmazonSignerService.getAmazonQueryString(query);
-    };
+//     const searchProducts = (query) => {
+//         return $http.get(getAmazonQueryString(query));
+//         );
+//     };
 
-    // const searchProducts = (query) => {
-    //     return $http.get()
-    // }
-    // const searchProducts = (query) => {
-    //     return $http.get(
-    //         `http://webservices.amazon.com/onca/xml?Service=AWSECommerceService%0AOperation=ItemSearch&AWSAccessKeyId=${}&AssociateTag=${}&Keywords=${query}&Timestamp=[YYYY-MM-DDThh:mm:ssZ]&Signature=${}`
-    //     )
-    // };
+//     return {searchProducts};
 
-    return {searchProducts};
-
-});
+// });
