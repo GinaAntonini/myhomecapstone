9ee77aa556d55c89c3fd1d9274eadfa957625e81
Gina Antonini <ginamantonini@gmail.com>
2017-12-15 23:15:50 -0600

    added buttons to dashboard view

--- javascripts/services/MaterialsService.js
+++ javascripts/services/MaterialsService.js
@@ -1,36 +1,34 @@
 "use strict";
 
 app.service("MaterialsService", function($http, $q, FIREBASE_CONFIG){
-    // const getSelectedMaterials = (userUid) => {
-	// 	let materials = [];
-	// 	return $q((resolve, reject) => {
-	// 		$http.get(`${FIREBASE_CONFIG.databaseURL}/materials.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
-	// 			let fbMaterials = results.data;
-	// 			Object.keys(fbMaterials).forEach((key) =>{
-	// 				fbMaterials[key].id = key;  // fbMovies["movies0"].id = "movies0"
-	// 				if(!fbMMaterials[key].isWatched){
-	// 					movies.push(fbMovies[key]);
-	// 				}
-	// 				resolve(movies);
-	// 			});
-	// 		}).catch((err) => {
-	// 			reject(err);
-	// 		});
-	// 	});
-    // };
+    const getSelectedMaterials = (userUid) => {
+		let materials = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/materials.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
+				let fbMaterials = results.data;
+				Object.keys(fbMaterials).forEach((key) =>{
+					fbMaterials[key].id = key;  // fbMovies["movies0"].id = "movies0"
+						materials.push(fbMaterials[key]);
+					resolve(materials);
+				});
+			}).catch((err) => {
+				reject(err);
+			});
+		});
+    };
     
-    // const createItemObject = (item) => {
-    //     return {
-    //         "image": item.mediumImage,
-    //         "name": item.name,
-    //         "salePrice": item.salePrice,
-    //         "uid": item.uid
-    //     };
-    // };
+    const createItemObject = (item) => {
+        return {
+            "image": item.mediumImage,
+            "name": item.name,
+            "salePrice": item.salePrice,
+            "uid": item.uid
+        };
+    };
     
-    // return{
-    //     getSelectedMaterials,
-    //     createItemObject
-    // };
+    return{
+        getSelectedMaterials,
+        createItemObject
+    };
 
 });
