e022cde4784d39b061eccda98be46d3892c1bf07
Gina Antonini <ginamantonini@gmail.com>
2017-12-15 23:30:40 -0600

    added dashboard functionality

--- javascripts/controllers/DashboardCtrl.js
+++ javascripts/controllers/DashboardCtrl.js
@@ -1,7 +1,32 @@
 "use strict";
 
 app.controller("DashboardCtrl", function($location, $rootScope, $scope){
+
+    $scope.getToCreateANewMaintenanceTask = () => {
+        $location.path("/create/newtask");
+    };
+
+    $scope.getToCurrentMaintenanceItemsList = () => {
+        $location.path("/maintenance/viewtasks");
+    };
+
+    $scope.getToSeeTasksBySeason = () => {
+        $location.path("/maintenance/seasonalview");
+    };
+
     $scope.getToSearchProducts = () => {
         $location.path("/searchproducts");
+    };
+
+    $scope.getToCreateANewImprovementProject = () => {
+        $location.path("/create/newproject");
+    };
+
+    $scope.getToCurrentImprovementProjectsList = () => {
+        $location.path("/improvements/viewprojects");
+    };
+
+    $scope.getToSeeProjectsBySeason = () => {
+        $location.path("/improvements/seasonalview");
     };
 });
